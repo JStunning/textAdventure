c689879b6d23aca64f53de9587284600d1ef3505
Jack Dunning <JackStunning9001@gmail.com>
2020-01-17 10:08:03 -0800

    Add dev branch and some js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  $("#formBox").submit(function(event){
+
+    // Variables
+    var name = $("#aNameInput").val();
+    var aClass = $("#aClass").val();
+
+    // Conditionals
+
+
+
+
+
+  })
+
+
+})
